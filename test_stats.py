6b31dcc98d6b1ff2a767e525dd72885e298bbd13
apellis <apellis@wisc.edu>
2015-08-27 13:30:28 -0500

    added a third test

--- test_stats.py
+++ test_stats.py
@@ -4,10 +4,13 @@ from stats import mean
 #write a test fcn
 def test_mean():
         #checks to see if statement is true
-        assert(mean([2,4]) ==3 )
-
+        assert(mean([2,4])==3)
 test_mean()
 
 def test_float_mean():
         assert(mean([1,2]) == 1.5)
 test_float_mean()
+
+def test_neg_mean():
+	assert(mean([-2,2,4]) == 1.333)
+test_neg_mean()
