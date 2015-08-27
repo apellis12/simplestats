c5edb544dab0d3287320d3de3b086b45ee887baa
apellis <apellis@wisc.edu>
2015-08-27 13:46:25 -0500

    tried debugging test_stats.py

--- test_stats.py
+++ test_stats.py
@@ -6,13 +6,15 @@ from nose.tools import assert_equal
 def test_mean():
         #checks to see if statement is true
         #assert(mean([2,4])==3)
-        assert_equal(mean([2,4]),3)
+        assert_equal(mean([2.0,4.0]),3)
 #test_mean()
 
 def test_float_mean():
-        assert(mean([1,2]) == 1.5)
+        #assert(mean([1,2]) == 1.5)
+        assert_equal(mean([1.0,2.0]), 1.5)
 #test_float_mean()
 
 def test_neg_mean():
-	assert(mean([-2,2,4]) == 1.333)
+	#assert(mean([-2,2,4]) == 1.333)
+	assert_equal(mean([-2.0,2.0,4.0]), 1.333)
 #test_neg_mean()
