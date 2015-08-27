fe339198fc620e8bc1cf22448e04e059392121fd
apellis <apellis@wisc.edu>
2015-08-27 14:28:55 -0500

    first final version

--- stats.py
+++ stats.py
@@ -5,9 +5,16 @@ def mean(vals):
 	return float(total)/length
 
 def std(vals):
-	if len(vals) == 0:
+	n = len(vals)
+	if n == 0:
 		return 0.0
-	return vals[-1]/2.0
+	#mu = sum(vals)/n
+	mu = mean(vals)
+	var = 0.0
+	for val in vals:
+		var = var + (val-mu)**2
+	return (var/n)**0.5
+
 
 # quick example to call fcn
 print mean([2,4])
