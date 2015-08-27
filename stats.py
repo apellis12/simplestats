00db22472254f1678e1e293cfb941914a610a3a7
apellis <apellis@wisc.edu>
2015-08-27 14:12:56 -0500

    starting to add section function that uses std fcn

--- stats.py
+++ stats.py
@@ -4,6 +4,9 @@ def mean(vals):
 	length = len(vals)
 	return float(total)/length
 
+def std(vals):
+	return 1.0
+
 # quick example to call fcn
 print mean([2,4])
 
--- test_stats.py
+++ test_stats.py
@@ -1,5 +1,5 @@
 
-from stats import mean
+from stats import mean, std
 from nose.tools import assert_equal, assert_almost_equal
 
 
@@ -20,4 +20,8 @@ def test_neg_mean():
 	assert_almost_equal(mean([-2.0,2.0,4.0]), 1.333, places=3)
 #test_neg_mean()
 
+def test_std1():
+	obs = std([0.0,2.0])
+	exp = 1.0
+	assert_equal(obs,exp)
 
